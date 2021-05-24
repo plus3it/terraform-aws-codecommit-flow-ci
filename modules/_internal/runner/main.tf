d806707fcd4ffde8d405282c52036bb8299b7b85
Karri Balk <kbalk@users.noreply.github.com>
2021-05-24 11:28:03 -0400

    Update image to standard:4.0

--- modules/_internal/runner/main.tf
+++ modules/_internal/runner/main.tf
@@ -19,7 +19,7 @@ locals {
   # Setup the CodeBuild environment object
   default_environment = {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/nodejs:8.11.0"
+    image        = "aws/codebuild/standard:4.0"
     type         = "LINUX_CONTAINER"
   }
 
